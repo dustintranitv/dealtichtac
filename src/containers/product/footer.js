87dc3dcb93d2c8bd20020e925ea99acb05ad8438
dustintranitv <nhantt@innotech-vn.com>
2023-08-14 16:22:33 +0700

    [ADD] HTML To Reactjs

---
+++ src/containers/product/footer.js
@@ -0,0 +1,228 @@
+import React from "react";
+const FooterProduct = () => {
+  return (
+  <div>
+    <div className="product_bottom">
+      <ul className="product_bottom_all">
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/pin-du-phong.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/pin-du-phong.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">PIN DỰ PHÒNG</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/usb.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/usb.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">USB</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/tai-nghe.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/tai-nghe.png"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">TAI NGHE</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/loa-nghe-nhac.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/loa-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">LOA SIÊU ĐỘC</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/cap-sac-ket-noi-du-lieu.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/day-cap-sac.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">
+                CÁP SẠC ĐỦ LOẠI
+              </div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/bat-lua.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/batlua.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">BẬT LỬA</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/bao-da-op-lung.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/bao-da-2.png"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">
+                ỐP LƯNG, BAO DA
+              </div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/ban-phim.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/ban-phim-2.png"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">BÀN PHÍM</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/bo-phat-wifi.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/bo-thu-phat-wifi-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">BỘ PHÁT WIFI</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/balo.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/balo-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">BALO</div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/gay-chup-hinh-selfie.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/de-dung-dien-thoai-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">
+                GẬY CHỤP HÌNH SELFIE
+              </div>
+            </a>
+          </div>
+        </li>
+        <li className="product_bottom_all_item">
+          <div className="product_bottom_all_content">
+            <a
+              className="product_bottom_all_link"
+              href="https://www.dealtichtac.com/dong-ho.html"
+            >
+              <div>
+                <img
+                  className="product_bottom_all_item_img"
+                  src="https://www.dealtichtac.com/img/dong-ho-thong-minh-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="product_bottom_all_item_name">SMART WATCH</div>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div className="product_menu_bottom">
+      <ul
+        className="product_menu_bottom_item"
+        id="product_menu_bottom_item_id"
+      ></ul>
+    </div>
+  </div>
+  )
+};
+export default FooterProduct;
